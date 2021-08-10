3f7ca556aa21508f934bb4fd8de43ee118a54d12
竹若泠 <3489382552@qq.com>
2021-08-10 10:17:17 +0800

    removed and added apdlog

--- Helium/Helium.cpp
+++ Helium/Helium.cpp
@@ -18,8 +18,7 @@
 #include"xmlmacros.h"
 #include"minecraftserver.h"
 #include"extension.h"
-#define REPLXX_STATIC
-
+#include<spdlog/spdlog.h>
 using namespace std;
 
 
@@ -622,6 +621,19 @@ int _stdcall main()
     string pns = PROJECT_NAME_STR;
     pns.append(" ").append(PROJECT_VER_STR).append(" ").append(PROJECT_DEVSTAT);
     cout << pns << endl;
+    spdlog::info("Welcome to spdlog!");
+    spdlog::error("Some error message with arg: {}", 1);
+
+    spdlog::warn("Easy padding in numbers like {:08d}", 12);
+    spdlog::critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+    spdlog::info("Support for floats {:03.2f}", 1.23456);
+    spdlog::info("Positional args are {1} {0}..", "too", "supported");
+    spdlog::info("{:<30}", "left aligned");
+
+    spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+    spdlog::debug("This message should be displayed..");
+    
+
     if (auto ret = Config(); ret != 0) {
         print("Failed to read config");
         return -1;
