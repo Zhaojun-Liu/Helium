a35ca0dc989e3439a54a1ed0578c5e7627a509f4
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2021-10-05 01:23:20 +0800

    events

--- Helium/events.h
+++ Helium/events.h
@@ -4,12 +4,48 @@
 #define _H_EVENTS
 
 #include<deque>
+#include<map>
 #include<string>
+#include<vector>
+#include<guiddef.h>
 
 using namespace std;
 
-struct HeliumEvent
-{
+typedef int (_stdcall* HeliumEventCallbackType)(int events);
+
+class HeliumEvent;
+class HeliumEventInstance;
+
+deque<HeliumEventInstance> eventsqueue;
+map<int, HeliumEvent> eventmap;
+
+class HeliumEvent {
+public:
+	struct CallbackFunctionInfo {
+		GUID funcguid;
+		HeliumEventCallbackType funcptr;
+		bool funccallbackenable;
+
+		CallbackFunctionInfo();
+	};
+	HeliumEvent();
+
+	int RegisterListenerFunction(HeliumEventCallbackType funcptr);
+	CallbackFunctionInfo GetListenerFunction(LPGUID guid);
+	int DeleteListenerFunction(LPGUID guid);
+	void EnableCallback();
+	void DisableCallback();
+	void EnableFunctionCallback(LPGUID guid);
+	void DisableFunctionCallback(LPGUID guid);
+protected:
+	string eventname;
+	string eventdesc;
+	bool callbackenable;
+
+	vector<CallbackFunctionInfo> listenerfuncs;
+};
+
+class HeliumEventInstance {
 
 };
 
