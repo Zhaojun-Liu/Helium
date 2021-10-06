7cc42d4c803b2cee596c7ecc6336f93767391248
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2021-10-06 23:59:22 +0800

    emm

--- Helium/exportfunction.h
+++ Helium/exportfunction.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#ifndef _H_EXPORTFUNCTION
+#define _H_EXPORTFUNCTION
+
+#include<map>
+#include<string>
+
+extern std::map<std::string, void*> HeliumExportFunctionMap;
+int InitFuncMap();
+
+#endif // !_H_EXPORTFUNCTION
