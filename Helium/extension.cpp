03668b93a3a82020e9391dad38b907051b3e0387
Minecraft1248 <minecraft1248_bilibili@outlook.com>
2021-11-13 21:00:52 +0800

    咕

--- Helium/extension.cpp
+++ Helium/extension.cpp
@@ -1,116 +1,18 @@
 #include"extension.h"
 namespace Helium {
-
-	_stdcall HeliumExtension::HeliumExtension() {
-
-	}
-	_stdcall HeliumExtension::HeliumExtension(const HeliumExtension* ext) {
-
-	}
-	_stdcall HeliumExtension::~HeliumExtension() {
-
-	}
-
-	string _stdcall HeliumExtension::SetExtName(string name) {
-		this->extname = name;
-		return name;
-	}
-	string _stdcall HeliumExtension::GetExtName() {
-		return this->extname;
-	}
-
-	string _stdcall HeliumExtension::SetExtFileName(string name) {
-		this->extfilename = name;
-		return name;
-	}
-	string _stdcall HeliumExtension::GetExtFileName() {
-		return this->extfilename;
-	}
-
-	int    _stdcall HeliumExtension::SetExtStatus(int stat) {
-		this->extstatus = stat;
-		return stat;
-	}
-
-	void* _stdcall HeliumExtension::GetFuncPtr(const char* lpFuncName)
-	{
-		if (this->exthandle == NULL)
-		{
-			return NULL;
+	int HeliumExtensionConfig::ReadConfig()	{
+		tinyxml2::XMLDocument doc;
+		if (auto ret = doc.LoadFile(this->configpath.c_str())
+			; ret != tinyxml2::XMLError::XML_SUCCESS) {
+			spdlog::critical("Failed to load extension config file : " + this->configpath);
+			return -1;
 		}
-		void* ptr;
-		ptr = GetProcAddress(this->exthandle, lpFuncName);
-		return ptr;
-	}
 
-	int    _stdcall HeliumExtension::GetExtStatus() {
-		return this->extstatus;
-	}
-
-	int   _stdcall HeliumExtension::SetExtGUID(GUID guid) {
-		this->extguid = guid;
-		return 0;
-	}
-
-	int   _stdcall HeliumExtension::GetExtGUID(LPGUID lpGuid) {
-		*lpGuid = this->extguid;
-		return 0;
-	}
-
-	int    _stdcall HeliumExtension::LoadExt() {
-		if (this->extstatus == EXT_STATUS_EMPTY) {
-			this->extstatus = EXT_STATUS_LOADING;
-
-			char cwd[MAX_PATH] = { 0 };
-			string extpath;
-			FARPROC tempptr;
-
-			GetCurrentDirectoryA(MAX_PATH, cwd);
-			extpath.append(cwd).append("\\").append("extensions").append("\\").append(this->extfilename);
-
-			auto dllhandle = LoadLibraryA(extpath.c_str());
-			if (dllhandle == NULL)
-			{
-				return -2;
-			}
-			else
-			{
-				//GET_EXT_FUNCPTR("OnExtensionLoad");
-				//GET_EXT_FUNCPTR("OnServerStartup");
-				pair<int, void*> p;
-				this->exthandle = dllhandle;
-				fd(ON_LOAD, "OnLoad")
-					ins
-					fd(ON_INFO, "OnInfo")
-					ins
-					fd(ON_SERVER_LAUNCH, "OnServerLaunch")
-					ins
-					fd(ON_SERVER_START, "OnServerStart")
-					ins
-					fd(ON_SERVER_STARTUP, "OnServerStartup")
-					ins
-					fd(ON_SERVER_STOP, "OnServerStop")
-					ins
-					fd(ON_CONSOLE_INPUT, "OnConsoleInput")
-					ins
-					fd(ON_PLAYER_INFO, "OnPlayerInfo")
-					ins
-					fd(ON_UNLOAD, "OnUnload")
-					ins
-					fd(ON_HELIUM_STOP, "OnHeliumStop")
-					ins
-			}
-
-
-			this->extstatus = EXT_STATUS_LOADED;
-			return 0;
+		tinyxml2::XMLElement* root = doc.RootElement();
+		if (root == NULL) {
+			spdlog::critical("Failed to get root element of extension config file : " + this->configpath);
+			return -1;
 		}
-		return -1;
-	}
-	int    HeliumExtension::UnloadExt(bool forceunload) {
-		return 0;
-	}
-	int    HeliumExtension::Expire() {
 		return 0;
 	}
 }
--- Helium/extension.h
+++ Helium/extension.h
@@ -8,75 +8,66 @@
 #include<iostream>
 #include<map>
 #include<Windows.h>
-#include<guiddef.h>
+#include<spdlog/spdlog.h>
+#include"xmlutils.h"
+#include"events.h"
+#include"tinyxml2.h"
+
+using namespace std;
+
 namespace Helium {
+	class HeliumExtensionConfig {
+	public:
+		_declspec(property(get = GetExtConfigPath, put = PutExtConfigPath)) string Extconfigpath;
+		_declspec(property(get = GetExtName, put = PutExtName)) string Extname;
 
-#define EXT_STATUS_EMPTY 0
-#define EXT_STATUS_LOADING 1
-#define EXT_STATUS_LOADED 2
-#define EXT_STATUS_UNLOADING 3
-#define EXT_STATUS_EXPIRED 4
+		string _stdcall GetExtConfigPath() {
+			return configpath;
+		}
+		void _stdcall PutExtConfigPath(string path) {
+			this->configpath = path;
+		}
 
-#define ON_LOAD				0x1
-#define ON_INFO				0x10
-#define ON_SERVER_LAUNCH	0x1001
-#define ON_SERVER_START		0x100
-#define ON_SERVER_STARTUP	0x1000
-#define ON_SERVER_STOP		0x10000
-#define ON_CONSOLE_INPUT	0x100000
-#define ON_PLAYER_INFO		0x1000000
-#define ON_UNLOAD			0x10000000
-#define ON_HELIUM_STOP		0x20000000
+		string _stdcall GetExtName() {
+			return configpath;
+		}
+		void _stdcall PutExtName(string path) {
+			this->configpath = path;
+		}
 
+		int ReadConfig();
+	private:
+		string configpath;
+		string Extname;
+	};
 
-#define GET_EXT_FUNCPTR(name) tempptr = GetProcAddress(dllhandle, name);\
-if(tempptr == NULL) {\
-	cout << "Get extension function pointer failed : " << name << endl;\
-	return -1;\
-}\
-this->extfuncptrcache.push_back(tempptr)
-#define awa return 0
-#define retstr string ret = "";return ret
-#define ins this->extfunccache.insert(p);
-#define fd(a,b) p.first = a;p.second = this->GetFuncPtr(b);
-	using namespace std;
+	class HeliumExtensionFuncs {
+		
+	};
 
 	class HeliumExtension {
-	private:
-		string		extname;
-		string		extfilename;
-
-		int			extstatus = 0;
-
-		GUID		extguid;
-
-		HMODULE		exthandle = NULL;
-
-		map<int, void*>	extfunccache;
+		friend class HeliumExtensionConfig;
+		friend class HeliumExtensionFuncs;
 
 	public:
-		_stdcall HeliumExtension();
-		_stdcall HeliumExtension(const HeliumExtension* ext);
-		_stdcall ~HeliumExtension();
+		int _stdcall LoadExt();
+		int _stdcall LockExt();
+		int _stdcall UnlockExt();
+		int _stdcall UnloadExt();
+		int _stdcall SetConfigPath(string path);
 
-		string _stdcall SetExtName(string name);
-		string _stdcall GetExtName();
-
-		string _stdcall SetExtFileName(string name);
-		string _stdcall GetExtFileName();
-
+		enum ExtStat {
+			EXT_STATUS_EMPTY,
+			EXT_STATUS_LOADING,
+			EXT_STATUS_LOADED,
+			EXT_STATUS_UNLOADING,
+			EXT_STATUS_LOCKED
+		};
 	private:
-		int    _stdcall SetExtStatus(int stat);
-		void* _stdcall GetFuncPtr(const char* UbgDvtcfVY);
-	public:
-		int    _stdcall GetExtStatus();
-
-		int    _stdcall SetExtGUID(GUID guid);
-		int    _stdcall GetExtGUID(LPGUID lpGuid);
-
-		int    _stdcall LoadExt();
-		int    _stdcall UnloadExt(bool forceunload = false);
-		int    _stdcall Expire();
+		HeliumExtensionConfig config;
+		HeliumExtensionFuncs funcs;
+		int extstat;
+		vector<HeliumEventInstance*> EventQueue;
 	};
 }
 #endif //!_H_EXTENSION
